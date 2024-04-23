64aa3cfe31e14483d3904cff278eea8b1a517d9b
mzmarshall1278 <mzmarshall1278@gmail.com>
2024-04-23 16:19:27 +0100

    about page

---
+++ src/app/components/AboutUs.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AboutUs() {
+  return (
+    <section className='mt-24 bg-teal-900 py-24'>
+        <h1 className="text-5xl font-semibold text-gray-200 text-center">About Us</h1>
+        <p className="w-2/3 text-center mx-auto text-gray-100 mt-4 leading-loose">We are Sight Boost, An Optical CLinic with unlimited superpowers. We are an organisation founded by Dr Anas Muhammad. An Optician in Kano and a couple of individuals from Abuja. We are Sight Boost, An Optical CLinic with unlimited superpowers. We are an organisation founded by Dr Anas Muhammad. An Optician in Kano and a couple of individuals from Abuja.</p>
+    </section>
+  )
+}
