7c31ecd88f56e794012c5aa3dffa58008fb7045c
mzmarshall1278 <mzmarshall1278@gmail.com>
2024-05-23 22:25:03 +0100

    .

--- src/app/components/ImageLoader.tsx
+++ src/app/components/ImageLoader.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function ImageLoader({url, close}: {url: string, close: ()=> void}) {
   return (
     <div className="w-screen h-screen fixed top-0 left-0 z-40 flex flex-col justify-between align-middle my-auto p-4 sm:p-24 bg-slate-800/90">
-        <div className="absolute top-8 w-full z-20" onClick={()=> close()}>
+        <div className="absolute top-20 w-full z-20" onClick={()=> close()}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
