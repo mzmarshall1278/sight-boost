40dd133600e35bb82728e16d5670bf9a90bea632
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-07-02 10:03:37 +0100

    display loggin user

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import React from 'react'
 import Login from '../components/Admin/Login'
+import { useSession } from 'next-auth/react';
 
 const page = () => {
+
+  const { data: session } = useSession();
+
   return (
     <div>
+      {session ?(
+        <div>User is logged in</div>
+      ):(
         <Login/>
+      )}
     </div>
   )
 }
