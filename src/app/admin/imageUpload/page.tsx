6641c8120a4f0871c37e9943e7c296bcd9c80547
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-07-04 11:43:12 +0100

    check admin session

--- src/app/admin/imageUpload/page.tsx
+++ src/app/admin/imageUpload/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import Form from '@/app/components/Admin/Form'
+import Login from '@/app/components/Admin/Login';
+import { useSession } from 'next-auth/react';
 import React from 'react'
 
 const page = () => {
+  const { data: session } = useSession();
+
   return (
     <div>
+      {session?.user ? (
         <Form/>
+      ):(
+        <Login/>
+      )}
     </div>
   )
 }
