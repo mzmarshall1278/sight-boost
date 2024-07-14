724868d6ed6c33d08ee6ff0f92c08a5c0a2172c1
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-07-14 20:06:13 +0100

    testing and saving user data

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,15 +3,22 @@
 import React from 'react'
 import Login from '../components/Admin/Login'
 import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 const page = () => {
 
   const { data: session } = useSession();
+  const userName = session?.user?.name
 
   return (
     <div>
       {session ?(
-        <div>User is logged in</div>
+        <>
+          <div>Welcome {userName}</div>
+          <Link href='/admin/imageUpload'>
+            <button className='bg-black rounded-xl text-md p-2 text-white'>Upload Image</button>
+          </Link>
+        </>
       ):(
         <Login/>
       )}
