bf5adf2083fa01b91db07823be43fe5620dd5db5
mzmarshall1278 <mzmarshall1278@gmail.com>
2024-07-16 16:44:11 +0100

    add final touches

--- src/app/components/Admin/Form.tsx
+++ src/app/components/Admin/Form.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import { imageOptions } from '../../../../utils/rawData';
 import SuccessModal from './SuccessModal';
 import ErrorModal from './ErrorModal';
+import { useRouter } from 'next/navigation';
 
   
 const Form = () => {
@@ -17,6 +18,8 @@ const Form = () => {
         imageCategory: "",
         imageUrl: "",
     });
+
+    const router = useRouter()
 
     const type = 'submit';
     
@@ -58,6 +61,7 @@ const Form = () => {
                 const data = await response.json();
                 console.log('Course created successfully:', data);
                 setSuccessMsg(data?.message);
+                router.push('/gallery')
             } else {
                 const errorData = await response.json();
                 console.error('Failed to create a new course:', errorData);
@@ -78,7 +82,7 @@ const Form = () => {
                 onSubmit={handleSubmit}
             >
                     <div>
-                        <label className="md:text-center text-left block text-sm font-medium text-gray-700">
+                        <label className="md:text-center text-left block text-xl mb-4 font-semibold text-teal-900">
                             <span className="text-red-600">*</span>
                             Select Image Category
                         </label>
@@ -90,24 +94,24 @@ const Form = () => {
                         >
                             <option value="" disabled className='cursor-not-allowed'>Select a Category</option>
                             {imageOptions.map(option => (
-                            <option key={option.value} value={option.value}>{option.label}</option>
+                            <option className=' ' key={option.value} value={option.value}>{option.label}</option>
                             ))}
                         </select>
                         </div>
-                <label className="flex flex-col w-full items-center justify-center mx-auto border-2 border-gray-600 p-4">
-                    <span className="font-satoshi font-semibold text-base text-gray-700">
+                <label className="flex flex-col w-full items-center justify-center mx-auto border-2 rounded-xl bg-teal-900 p-4">
+                    <span className="font-satoshi text-base text-white font-bold mb-4">
                         Upload Image{' '}
                     </span>
 
                     <CldUploadButton
-                    className=' cursor-pointer w-fit px-4 py-2 text-xl bg-orange-600 hover:bg-white hover:text-gray-700 border border-primary-orange rounded-md text-white'
+                    className=' cursor-pointer w-fit px-4 py-2 text-xl bg-black hover:bg-white hover:text-gray-700 border border-primary-orange rounded-md text-white'
                         uploadPreset="miva-test"
                         onSuccess={handleUpload}
                     />
 
                     {imageId && (
-                        <div className="rounded-lg overflow-hidden w-[20rem] h-[20rem] relative">
-                            <Image src={imageId} fill alt="image" className="object-cover rounded-lg" />
+                        <div className="rounded-lg overflow-hidden size-full md:w-[20rem] h-[20rem] relative mt-4">
+                            <Image src={imageId} fill alt="image" className="object-contain rounded-lg" />
                         </div>
                     )}
                 </label>
@@ -115,7 +119,7 @@ const Form = () => {
                     <button
                         type='submit'
                         disabled={submitting}
-                        className='px-5 py-1.5 text-sm bg-black rounded-full text-white'
+                        className='px-5 py-2 text-lg bg-teal-900 rounded-full text-white w-full md:w-2/3 mx-auto'
                     >
                         {submitting ? `${type}ing...` : type}
                     </button>
