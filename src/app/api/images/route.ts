1eb86388fab7017b4cd8c3d3828ece59a0febaaf
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-07-17 20:41:18 +0100

    connection to db

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -1,7 +1,11 @@
 import ImageUpload from "../../../../models/ImageUpload"
+import { connectToDB } from "../../../../utils/database";
 
 export const GET = async (request: any) => {
     try {
+
+        await connectToDB();
+
         const images = await ImageUpload.find({})
 
         return new Response(JSON.stringify(images), { status: 200 })
